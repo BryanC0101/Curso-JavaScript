cd2b5497a36192b18bc66c97b26823bf089def81
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-04-26 17:22:05 -0300

    it is working, but has a bug on css container height

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -4,8 +4,26 @@ function addTask() {
     if (textArea.value != "") {
         let tasks = document.querySelector("ul");
         let li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.textContent = "X";
+
+        btn.addEventListener("click", ()=> {
+            li.remove();
+        })
+
+        li.addEventListener("click", ()=> {
+            if (li.style.textDecoration === "line-through") {
+                li.style.background = "white";
+                li.style.textDecoration = "none";
+            }else{
+                li.style.background = "#CAE7B9";
+                li.style.textDecoration = "line-through";
+            }
+        })
+
         li.textContent = textArea.value;
         tasks.appendChild(li);
+        li.appendChild(btn);
         textArea.value = "";
     }
 
